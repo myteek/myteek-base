c7280bacfecdb55b9cc36c7bfd9283c73d698f26
jake <ft20082@qq.com>
2022-04-28 16:26:40 +0800

    1. init commit

--- compile.sh
+++ compile.sh
@@ -3,3 +3,6 @@
 source /etc/profile
 
 mvn clean install package -DskipTests --settings ./settings.xml
+
+# deploy
+# mvn clean install package org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests --settings ~/.m2/aliyun.xml
